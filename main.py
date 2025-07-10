c2cb63d27f10f6b39209ce897c246f12fde19c18
Ewa Kruzynska <ekkruzynska@gmail.com>
2025-07-10 12:16:15 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+shopping_list = ["buraki","masło","chleb"]
+sorted_shopping_list = sorted(shopping_list)
+print(f"Lista po sortowaniu {sorted_shopping_list}")
+print(f"Lista pierwotna {shopping_list}")
